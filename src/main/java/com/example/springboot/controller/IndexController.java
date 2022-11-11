6df6e964d31806117616b1b7cf083e6f18697bc5
msw38 <msw8603@gmail.com>
2022-11-11 15:27:32 +0900

    init

--- src/main/java/com/example/springboot/controller/IndexController.java
+++ src/main/java/com/example/springboot/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.example.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class IndexController {
 
 
